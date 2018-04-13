798e1e8c6c230726b77a877637f15c5456c184c1
Patrick Amaral <patrick.adrp@gmail.com>
2018-04-12 21:54:45 -0300

    Criado função Uk.

--- RNA.py
+++ RNA.py
@@ -1,3 +1,5 @@
+import random
+
 __author__ = 'Patrick Amaral'
 
 def criarMatriz(local):
@@ -23,9 +25,29 @@ def criarMatriz(local):
 
     return matriz
 
+def uK(matriz):
+    wk1 = random.random()
+    wk2 = random.random()
+
+    print('WK1: {} WK2: {}'.format(wk1, wk2))
+    for j in range(len(matriz)):
+        sigma = 0
+        u=[]
+        for i in range(len(matriz[j])-1):
+            if i == 0:
+                result = matriz[j][i]*wk1
+                print('X1: {}'.format(result))
+            if i == 1:
+                result = matriz[j][i]*wk2
+                print('X2: {}'.format(result))
+            sigma=sigma+result
+        print('SIGMA: {}'.format(sigma))
+        u.append(sigma)
+    return u
 
 if __name__ == '__main__':
 
     local = str(input('Digite local do arquivo: '))
 
     print(criarMatriz(local))
+    uK(criarMatriz(local))
